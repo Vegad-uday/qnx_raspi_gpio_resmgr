14c6809395683240ff1a87e2028495c51cc6e834
Vegad-uday <vegaduday1292@gmail.com>
2025-09-07 15:23:40 +0530

    add condition handling

--- resorce_manager.c
+++ resorce_manager.c
@@ -1,39 +1,4 @@
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <errno.h>
-#include <devctl.h>
-#include <sys/iofunc.h>
-#include <sys/dispatch.h>
-#include <sys/mman.h>
-
-
-#define BCM2711_GPIO_BASE                   0xfe200000
-#define GPSET0    7
-#define GPLEV0	(0x34/4)
-#define GPCLR0    (0x28/4)
-#define BLOCK_SIZE	(4*1024)
-#define GPIO_SR		0x07
-#define OP	1
-#define IP	0
-
-
-struct gpio_info{
-	int pin;
-	int mode;
-	int state;
-	int state_read;
-}gpio;
-
-static uint32_t volatile *gpio_base;
-
-#define MSG_COMD __DIOT(_DCMD_MISC, 1, struct gpio_info)
-
-static int set_gpio_input(int gpio_pin);
-static int set_gpio_output(int gpio_pin);
-static int set_gpio_state(int gpio_pin , int state);
-static int gpio_read_func(int gpio_pin);
+#include "gpio_function.h"
 
 static int my_devctl_handler(resmgr_context_t *ctp, io_devctl_t *msg, RESMGR_OCB_T *ocb);
 
@@ -47,10 +12,9 @@ int main(int argc, char *argv[]) {
 
     // Create dispatch
     if ((dpp = dispatch_create()) == NULL) {
-        perror("dispatch_create");
+        printf("dispatch_create");
         exit(EXIT_FAILURE);
     }
-
     // Initialize default attributes
     memset(&resmgr_attr, 0, sizeof(resmgr_attr));
     resmgr_attr.nparts_max = 1;
@@ -63,84 +27,23 @@ int main(int argc, char *argv[]) {
 
     // Set up attributes
     iofunc_attr_init(&io_attr, S_IFCHR | 0666, NULL, NULL);
-
     // Attach resource manager to /dev/rpi_gpio
     if (resmgr_attach(dpp, &resmgr_attr, "/dev/rpi_gpio",_FTYPE_ANY, 0, &connect_funcs, &io_funcs, &io_attr) == -1) {
         printf("resmgr_attach failed\n");
         exit(EXIT_FAILURE);
-    }
-    //GPIO_BASE gets  physical address
-    gpio_base = mmap_device_memory(NULL,BLOCK_SIZE,PROT_NOCACHE|PROT_READ|PROT_WRITE,0,BCM2711_GPIO_BASE);
-    if (gpio_base == (uint32_t *) MAP_FAILED) {
-    	printf("ERROR :GPIO Memory map failed\n");
-    	return -1;
     }
 
     ctp = dispatch_context_alloc(dpp);
 
     while (1) {
         if ((ctp = dispatch_block(ctp)) == NULL) {
-            perror("dispatch_block");
+            printf("dispatch_block");
             exit(EXIT_FAILURE);
         }
         dispatch_handler(ctp);
     }
 
     return 0;
-}
-
-//function for set gpio as input output
-static int set_gpio_input(int gpio_pin) {
-    int reg = gpio_pin / 10;
-    int sel = gpio_pin % 10;
-    int val = *(gpio_base + reg );
-
-    val &= ~(GPIO_SR << (3 * sel));
-    val |= ( IP & GPIO_SR) << (3*sel);
-    *(gpio_base + reg )=val;
-
-    return 0;
-}
-//function for set gpio as  output
-static int set_gpio_output(int gpio_pin) {
-
-    int reg = gpio_pin / 10;
-    int sel = gpio_pin % 10;
-    int val = *(gpio_base + reg );
-
-    val &= ~(GPIO_SR << (3 * sel));
-    val |= ( OP & GPIO_SR) << (3*sel);
-    *(gpio_base + reg )=val;
-
-    return 0;
-}
-
-//function for set pin high and low
-static int set_gpio_state(int gpio_pin , int state) {
-
-    if (state != 0) {
-    	*(gpio_base + GPSET0) = (uint32_t)(0x1 << gpio_pin);
-    	printf("PIN %d SET OUTPUt HIGH\n",gpio_pin);
-    }
-    else {
-    	*(gpio_base + GPCLR0) = (uint32_t)(0x1 << gpio_pin);
-    	printf("PIN %d SET OUTPUT LOW\n",gpio_pin);
-    }
-    return 0;
-}
-
-//function for gpio read
-static int gpio_read_func(int gpio_pin){
-	int gpio_level =*(gpio_base + GPLEV0);
-	gpio_level &= (1 << gpio_pin);
-	if (gpio_level == 0) {
-		printf("PIN %d is LOW\n",gpio_pin);
-		return 0;
-	}
-	else {
-		printf("PIN %d is HIGH\n",gpio_pin);
-		return 0;
-	}
 }
 
 
@@ -151,7 +54,7 @@ static int my_devctl_handler(resmgr_context_t *ctp, io_devctl_t *msg, RESMGR_OCB
         case MSG_COMD:
 
             resmgr_msgread(ctp, &gpio, sizeof(struct gpio_info), sizeof(msg->i));
-
+    printf("This is line %d of the program.\n", __LINE__);
             if (gpio.mode == 0) {
             	set_gpio_input(gpio.pin);
             	if (gpio.state_read == 1) {
@@ -165,14 +68,18 @@ static int my_devctl_handler(resmgr_context_t *ctp, io_devctl_t *msg, RESMGR_OCB
             	}
             }
             else if (gpio.mode == 1 ) {
+    printf("This is line %d of the program.\n", __LINE__);
             	set_gpio_output(gpio.pin);
             	if (gpio.state == 0) {
             		set_gpio_state(gpio.pin, gpio.state);
+    printf("This is line %d of the program.\n", __LINE__);
             	}
             	else if (gpio.state == 1) {
+    printf("This is line %d of the program.\n", __LINE__);
             		set_gpio_state(gpio.pin, gpio.state);
             	}
             	else {
+    printf("This is line %d of the program.\n", __LINE__);
             		printf("Invalid opration of GPIO\n");
             		return EIO;
             	}
--- user_app.c
+++ user_app.c
@@ -4,116 +4,142 @@
 #include <unistd.h>
 #include <devctl.h>
 #include <errno.h>
+#include <string.h>
+#include <stdint.h>
 
 #define GPIO_MIN 2
-#define GPIO_MAX  27
+#define GPIO_MAX 27
 
-struct gpio_info{
-	int pin;
-	int mode;
-	int state;
-	int state_read;
-}gpio;
+struct gpio_info {
+    int pin;
+    int mode;
+    int state;
+    int state_read;
+} gpio;
 
 #define MSG_COMD __DIOT(_DCMD_MISC, 1, struct gpio_info)
 
-
 int main(int argc, char *argv[]) {
 
-	gpio.mode = 2;
-	gpio.state = 2;
-	int check_arg_p;
-	int check_arg_o;
-	int check_arg_i;
-	int check_arg_s;
+    gpio.mode = -1;
+    gpio.state = -1;
+    gpio.state_read = 0;
+    
+
+    uint16_t pin_flag = 0;
+    uint16_t output_flag = 0;
+    uint16_t input_flag = 0;
+    uint16_t state_flag = 0;
 
     int fd = open("/dev/rpi_gpio", O_RDWR);
     if (fd == -1) {
         printf("ERROR FILE OPENING\n");
         return ENOENT;
     }
-    for (int i=1; i<argc; i++) {
-    	if (argv[i][0] == '-') {
-    		switch (argv[i][1]) {
-    			case 'p':
-    					check_arg_p =1;
-    					gpio.pin = atoi(&argv[i][2]);
-    					if (gpio.pin < GPIO_MIN || gpio.pin > GPIO_MAX ) {
-    					    printf("Argument -p is Invalid\n");
-    					    return EINVAL;
-    					}
-    					break;
-    			case 'i':
-    					gpio.mode = 0;
-    					check_arg_i =1;
-    					break;
-    			case 'o':
-    					gpio.mode = 1;
-    					check_arg_o =1;
-    					break;
-    			case 's':
-    					gpio.state = atoi(&argv[i][2]);
-    					if (!(gpio.state == 0 || gpio.state == 1) ) {
-    						printf("Argument %s is Invalid\n",argv[i]);
-    						return EINVAL;
-    					}
-    					check_arg_s =1;
-    					break;
-    			case 'r':
-    					gpio.state_read =1;
-    					if (!( gpio.state_read == 1 )) {
-    						printf("Argument %s is Invalid\n",argv[i]);
-    						return EINVAL;
-    					}
-    					break;
-    			default :
-    					printf("Arguments %d is Invalid\n",i);
-    					return EINVAL;
-    		}
-    	}
+
+    for (int i = 1; i < argc; i++) {
+        if (argv[i][0] == '-') {
+            switch (argv[i][1]) {
+                case 'p':
+		    if (argv[i][4] == 0){
+                        pin_flag = 1;
+                        gpio.pin = atoi(&argv[i][2]);
+		    }
+                    if (gpio.pin < GPIO_MIN || gpio.pin > GPIO_MAX) {
+                        printf("Argument %s is Invalid\n",argv[i]);
+                        return EINVAL;
+                    }
+                    break;
+                case 'i':
+		    if ( argv[i][2] == 0 ) {
+		        gpio.mode = 0;
+		        input_flag = 1;
+		    }
+		    else { 
+			printf("Argument %s is Invalid\n",argv[i]);
+			return EINVAL;
+		    }
+                    break;
+                case 'o':
+		    if (argv[i][2] == 0 ){
+                        gpio.mode = 1;
+                        output_flag = 1;
+		    }
+		    else {
+			printf("Argument %s is Invalid\n",argv[i]);
+			return EINVAL;
+		    }
+                    break;
+                case 's':
+		    if (argv[i][3]  == 0 ){
+		        if (argv[i][2] == 48 || argv[i][2] == 49) {
+                            gpio.state = atoi(&argv[i][2]);
+                            state_flag = 1;
+		        }
+		    }
+		    else {
+			    printf("Argument %s is Invalid\n",argv[i]);
+			    return EINVAL;
+		    }
+                    break;
+                case 'r':
+		    if (argv[i][2] == 0 ){
+                        gpio.state_read = 1;
+		    }else {
+			printf("Argument %s is Invalid\n",argv[i]);
+			return EINVAL;
+		    }
+                    break;
+                default:
+                    printf("Arguments %d is Invalid\n", i);
+                    return EINVAL;
+            }
+        }
     }
-    if (!(check_arg_p == 1) ){
-    	printf("Argument -p Missing\n");
-    	return EINVAL;
+
+    if (!(pin_flag == 1)) {
+        printf("Argument -p Missing\n");
+        return EINVAL;
     }
-    if (check_arg_p == 1 && !(gpio.mode == 0 || gpio.mode ==1 )) {
-    	printf("Pin mode not set \n");
-    	return EINVAL;
+
+    if (pin_flag == 1 && !(gpio.mode == 0 || gpio.mode == 1)) {
+        printf("Pin Mode Not Set \n");
+        return EINVAL;
     }
-    if (check_arg_s == 1 && !(check_arg_o  == 1)) {
-    	printf("Argument -s required -o\n");
-    	return EINVAL;
+
+    if (input_flag == 1 && output_flag == 1) {
+        printf("Input and Output Mode Not Use in single Command\n");
+        return EIO;
     }
-    if (!(check_arg_s == 1) && check_arg_o ==1) {
-    	printf("Argument -o required -s\n");
-    	return EINVAL;
+
+    if (state_flag == 1 && !(output_flag == 1)) {
+        printf("State High or Low  Need Output Argument\n");
+        return EINVAL;
     }
-//    if (!(gpio.mode == 0)) {
-//    	if (!(gpio.mode == 1 && (gpio.state == 0 || gpio.state ==1 ))) {
-//    		printf("Argument -o and -s not  \n");
-//        	return EIO;
-//     	}
-//     }
+
+    if (output_flag == 1 && state_flag == 0) {
+        printf("Output Mode Need Argument State High or Low\n");
+	return EINVAL;
+    }
+
     if (gpio.mode == 1 && gpio.state_read == 1) {
-    	printf("Argument -o not use with -r\n");
-    	return EPERM;
+        printf("Output Mode  Not Use with State Read(-r)\n");
+        return EPERM;
     }
+
     if (gpio.mode == 0 && (gpio.state == 0 || gpio.state == 1)) {
-    	printf("Argument -i not use with -s\n");
-    	return EPERM;//not opration permission
-    }
-    if (check_arg_i == 1 && check_arg_o ==1){
-    	printf("input and output both mode not set at time\n");
-    	return EIO;
+        printf("Input Mode Not Use with State High (-s1) or Low (-s0)\n");
+        return EPERM; 
     }
 
     int ret = devctl(fd, MSG_COMD, &gpio, sizeof(struct gpio_info), NULL);
 
     if (ret != 0) {
-        printf("devctl failed\n");
+        printf("devctl failed \n");
         return ENOTTY;
     }
 
     close(fd);
     return 0;
 }
+
